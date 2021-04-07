b122d9f2f848c5aa8728cf37f71e64ba627baf40
David Morilla Cabello <davidmorillacabello@gmail.com>
2021-04-07 20:23:52 +0200

    Lab 1 files: Exercise 2 Lab 1

---
+++ mrs_ex2_ws/src/ex2_rendezvous/scripts/position_plotter.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import sys
+from math import radians, copysign, sqrt, pow, pi, atan2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib import animation
+matplotlib.use("TkAgg")
+
+import rospy
+from std_msgs.msg import String
+from ex2_rendezvous.msg import queue_position_plot
+from ex2_rendezvous.srv import gossip_update, gossip_updateResponse
+
+
+class Plotter():
+    def __init__(self, n_robots):
+        # Class attributes
+        self.n_robots = n_robots
+        # Positions of all the robots. To make things easier, we assume that the robot ids are consecutive 1..n
+        # and that here we index them in the array with 0..n-1 respectively.
+        self.v_robot_x = np.zeros([n_robots])
+        self.v_robot_y = np.zeros([n_robots])
+        self.sub = rospy.Subscriber('queue_position_plot', queue_position_plot,
+        self.callback_position_received)
+        self.act_drawing()
+
+    def act_drawing(self):
+        #rospy.sleep(0.01) # give some time to receive at least a set of robot positions.
+        while not rospy.is_shutdown():
+            print('vamos')
+            plt.clf()
+            plt.xlim(-100,100)
+            plt.ylim(-100,100)
+            for i in range(0,n_robots):
+                plt.plot(self.v_robot_x[i],self.v_robot_y[i],'bo')
+            plt.draw()
+            plt.show(block=False)
+            plt.pause(0.01)
+            rospy.sleep(0.5) # twice per second. You can increase this rate
+    def callback_position_received(self, new_position):
+        #print(rospy.get_caller_id() + "I heard:")
+        #print('robot_id: ', new_position.robot_id)
+        #print('x: ', new_position.x)
+        #print('y: ', new_position.y)
+        self.v_robot_x[new_position.robot_id-1]=new_position.x
+        self.v_robot_y[new_position.robot_id-1]=new_position.y
+        print(self.v_robot_x)
+        print(self.v_robot_y)
+        
+if __name__ == '__main__':
+    #Input arguments (robot id, x0, y0, Tlocal, neig1, neig2... neign)
+    sysargv = rospy.myargv(argv=sys.argv) # to avoid problems with __name:= elements.
+    num_args=len(sysargv)
+    if (num_args >= 1):
+        n_robots = int(sysargv[1])
+    else:
+        n_robots = 1
+    try:
+        rospy.init_node('position_plotter', anonymous=False)
+        my_plotter=Plotter(n_robots)
+        print('Hello World!')
+        print('Printing the positions of ', my_plotter.n_robots)
+    except rospy.ROSInterruptException:
+        pass
---
+++ mrs_lab1_ws/src/lab1_deploy_gazebo_students/scripts/deployment_turtlebot_circle.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+import actionlib
+from std_msgs.msg import String
+from lab1_deploy_gazebo_students.srv import gossip_update, gossip_updateResponse
+from lab1_deploy_gazebo_students.msg import GoToGoal_goal, queue_position_plot
+import sys
+from math import radians, copysign, sqrt, pow, pi, atan2
+import numpy as np
+import tf
+import random
+
+class Robot():
+	def __init__(self, robot_id, num_robots, leader=False):
+    	# Class attributes 
+		self.robot_id = robot_id
+		self.leader = leader
+		self.num_robots = num_robots
+		self.theta = np.linspace(0, 2*np.pi, self.num_robots, endpoint=False)[self.robot_id]
+		self.t_local = 1/2.0
+		self.x = 0.0
+		self.y = 0.0
+		self.neighbors = []
+		self.available_neightbors = []
+		self.inter_distance = 1
+
+		# Obtain robot position
+		self.connected = False
+		self.tf_listener = tf.TransformListener()
+		while(not self.connected and not rospy.is_shutdown()):
+			try:
+				(trans, rot) = self.tf_listener.lookupTransform('tb3_{}/odom'.format(self.robot_id), 'tb3_{}/base_footprint'.format(self.robot_id), rospy.Time(0))
+				self.x = trans[0]
+				self.y = trans[1]
+				self.connected = True
+				print("Robot {} located: {} {}".format(self.robot_id, self.x, self.y))
+			except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+				print("Looking for {} robot position".format(self.robot_id))		
+			rospy.sleep(0.1)
+
+		self.pub = rospy.Publisher('topic_queue_position_plot', queue_position_plot, queue_size=10)
+		self.pub_goTogoal = rospy.Publisher('topic_GoToGoal_goal'+str(self.robot_id), GoToGoal_goal, queue_size=10)
+		self.ser = rospy.Service('gossip_update'+str(self.robot_id), gossip_update, self.handle_gossip_update)
+		
+		# Read params. Currently: neightbors and t_local. robot_id is arg and x,y are obtained from tf
+		self.read_parameters()
+		print("Initialized robot {}\n - Position: {} {}\n - Neightbours {} (requestable: {})\n - Local rate: {}"
+			.format(self.robot_id, self.x, self.y, self.neighbors, self.available_neightbors, self.t_local))
+		
+		if not self.leader:
+			self.timer = rospy.Timer(rospy.Duration(self.t_local), self.request_gossip_update)
+
+		rospy.sleep(0.5)
+		self.update_pose()
+		rospy.spin()
+		
+	def request_gossip_update(self, event):
+		print("Callback triggered {} times: {}".format(self.robot_id, self.counter))
+		
+		if self.available_neightbors is not None and self.available_neightbors:
+			target_id = random.choice(self.available_neightbors)
+		else:
+			return
+		
+		rospy.wait_for_service('gossip_update'+str(target_id)) # ask for service gossip_update
+		try:
+			print("{} request a service to {}".format(self.robot_id, target_id))
+			service_gossip_update = rospy.ServiceProxy('gossip_update'+str(target_id), gossip_update)
+			resp1=service_gossip_update(self.robot_id, self.x, self.y)
+
+			self.x=resp1.avg_x
+			self.y=resp1.avg_y
+			self.update_pose()
+
+		except rospy.ServiceException as e:
+			print("Service call failed: %s"%e)
+
+	def handle_gossip_update(self, req):
+		# Gossip and use of bx_ij, by_ij for deploying on a line
+		print("I am robot "+str(self.robot_id)+" and I received a gossip_update request: "+str(req.x)+","+str(req.y))
+		myResponse=gossip_updateResponse()
+		myResponse.avg_x=(req.x + self.x) / 2
+		myResponse.avg_y=(req.y + self.y) / 2
+		
+		self.x = myResponse.avg_x
+		self.y = myResponse.avg_y
+		self.update_pose()
+				
+		return myResponse
+
+	def update_pose(self):
+		print("{} publish at topic_queue_position_plot".format(self.robot_id))
+		my_pos_plot=queue_position_plot()
+		my_pos_plot.robot_id=self.robot_id
+		my_pos_plot.x=self.x + np.cos(self.theta)
+		my_pos_plot.y=self.y + np.sin(self.theta)
+		self.pub.publish(my_pos_plot)
+
+		print("{} publish a navigation goal at topicGoToGoal_goal".format(self.robot_id)+str(self.robot_id))
+		next_pos = GoToGoal_goal()
+		next_pos.goal_coords=[self.x  + np.cos(self.theta), self.y + np.sin(self.theta)]
+		next_pos.goal_z=0.0 #currently, not used
+		next_pos.speed=0.0 #currently, not used
+		self.pub_goTogoal.publish(next_pos)
+		#Up the here, endif
+
+	def read_parameters(self):
+		try:
+			#self.robot_id = int(rospy.get_param("~robot_id"))
+
+			# self.position = rospy.get_param("~position")
+			# if type(self.position) is str:
+			# 	self.position = [float(x)
+			# 						for x in self.position.split()]
+			
+			# self.x = self.position[0]
+			# self.y = self.position[1]
+
+			self.inter_distance = rospy.get_param("/inter_distance")
+
+			self.neighbors = rospy.get_param("~neightbors")
+			
+			if type(self.neighbors) is str:
+				self.neighbors = [int(x)
+									for x in self.neighbors.split()]
+				self.available_neightbors = []
+				for neightbor in self.neighbors:
+					if neightbor > self.robot_id:
+						self.available_neightbors.append(neightbor)
+			else:
+				self.neighbors = [self.neighbors]
+				if self.neighbors[0] > self.robot_id:
+					self.available_neightbors = self.neighbors
+				else:
+					self.available_neightbors = None
+
+			self.t_local = float(rospy.get_param("~t_local"))
+			return True
+		except rospy.ServiceException as e:
+			print("Parameters not set: "+str(e))
+			return False
+
+if __name__ == '__main__':
+	#Input arguments (robot id, x0, y0, Tlocal, neig1, neig2... neign)
+	sysargv = rospy.myargv(argv=sys.argv) # to avoid problems with __name:= elements.
+	num_args=len(sysargv)
+	if (num_args >= 1):
+		robot_id = int(sysargv[1])
+	else:
+		robot_id=0
+
+	if (num_args > 2):
+		num_robots = int(sysargv[2])
+	else:
+		num_robots=4
+
+	if (num_args > 3 and sysargv[3] == '--leader'):
+		leader = True
+	else:
+		leader = False
+	try:
+		rospy.init_node('deploy_'+str(robot_id), anonymous=False)
+		my_naive_robot=Robot(robot_id, num_robots, leader)
+		print('Finished!')
+
+	except rospy.ROSInterruptException:
+		pass
